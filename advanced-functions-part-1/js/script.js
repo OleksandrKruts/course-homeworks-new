b85e17db52a4b8f65b24bf532e5563b14040a8ef
OleksandrKruts <kruts704@gmail.com>
2026-04-18 23:24:51 +0200

    AF1: task 1 solution

--- advanced-functions-part-1/js/script.js
+++ advanced-functions-part-1/js/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+"use strict";
+
+//1. Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+//Для решения задачи необходим использовать рекурсию.
+
+function sumTo(n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n + sumTo(n - 1);
+  }
+}
+console.log(sumTo(4)); // 10
+console.log(sumTo(3)); // 6
+console.log(sumTo(1)); // 1
